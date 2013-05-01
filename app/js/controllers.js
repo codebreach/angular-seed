eb6a07c75f06e4e78a74dd4eeadfc9a18415d280
Madhav Bhagat <madhav89@gmail.com>
2013-05-01 01:30:24 -0400

    controller 2

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,32 +4,33 @@
 
 angular.module('app.controllers', []).
   controller('MyCtrl1', ['$scope', 'user', function(scope, user) {
-	scope.onclick = function() {
-		user.get('user', function(user) {
-			scope.user = user;
-			scope.$digest();
-		});
-	};
-	scope.save = function() {
-		var user_ = scope.user;
-		user.save(user_, function(user) {
-			scope.user = user;
-			scope.$digest();
-		});
-	};
-  }])
-  .controller('MyCtrl2', ['$scope', 'request', function() {
-  	scope.onclick = function() {
-		user.get('user', function(user) {
-			scope.user = user;
-			scope.$digest();
-		});
-	};
-	scope.save = function() {
-		var user_ = scope.user;
-		user.save(user_, function(user) {
-			scope.user = user;
-			scope.$digest();
-		});
-	};
-  }]);
+  scope.onclick = function() {
+    user.get('user', function(user) {
+      scope.user = user;
+      scope.$digest();
+    });
+  };
+  scope.save = function() {
+    var user_ = scope.user;
+    user.save(user_, function(user) {
+      scope.user = user;
+      scope.$digest();
+    });
+  };
+  }]).
+  controller('MyCtrl2', 
+    ['$scope', 'request', 'picture', function(scope, request, picture) {
+  
+  scope.onclick = function() {
+    request.create(scope.request, function(request) {
+      scope.request = request;
+      scope.$digest();
+    });
+  };
+  scope.upload = function() {
+    picture.create(scope.picture, function(picture) {
+      scope.picture = picture;
+      scope.$digets();
+    });
+  };
+}]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,59 +5,59 @@
 var publicKey = '2301fac2-e4e4-46ec-8061-7ebcb1d53698';
 
 angular.module('app.services', []).factory('model', function($rootScope) {
-	var service = {};
-	
-	var callbacks_ = function(callback) {
-		return {
-			success: function(model) {
-				service.model = model;
-				callback(model.toJSON());
-			},
-			error: function(reason) {
-				log(reason);
-			}
-		};
-	};
-	/**
-	 * @param {string=} id The id of the object
-	 * @param {function(!Object)=} callback 
-	 */
-	service.get = function(id, callback) {
-		var options = {};
-		options[schema + '_id'] = id;
-		service.model.set(options);
-		service.model.fetch(callbacks_(callback));
-	};
-	service.create = function(callback, json) {
-		service.model = new service.modelProvider(json);
-		service.model.create(callbacks(callback));
-	};
-	service.save = function(json, callback) {
-		service.model.save(json, callbacks_(callback));
-	};
+  var service = {};
+  
+  var callbacks_ = function(callback) {
+    return {
+      success: function(model) {
+        service.model = model;
+        callback(model.toJSON());
+      },
+      error: function(reason) {
+        log(reason);
+      }
+    };
+  };
+  /**
+   * @param {string=} id The id of the object
+   * @param {function(!Object)=} callback 
+   */
+  service.get = function(id, callback) {
+    var options = {};
+    options[schema + '_id'] = id;
+    service.model.set(options);
+    service.model.fetch(callbacks_(callback));
+  };
+  service.create = function(json, callback) {
+    service.model = new service.modelProvider(json);
+    service.model.create(callbacks(callback));
+  };
+  service.save = function(json, callback) {
+    service.model.save(json, callbacks_(callback));
+  };
   return function(schema, binaryFields) {
-	service.schema = schema;
-	service.modelProvider = StackMob.Model.extend({schemaName: schema});
-	service.model = new service.modelProvider();
-	return service;
+  service.schema = schema;
+  service.modelProvider = StackMob.Model.extend({schemaName: schema});
+  service.model = new service.modelProvider();
+  return service;
   };
 }).
 factory('user', function(model) {
-	return model('user');
+  return model('user');
 }).
 factory('comment', function(model) {
-	return model('comment');
+  return model('comment');
 }).
 factory('estimate', function(model) {
-	return model('estimate');
+  return model('estimate');
 }).
 factory('picture', function(model) {
-	return model('picture', 'data');
+  return model('picture', 'data');
 }).
 factory('request', function(model) {
-	return model('request');
+  return model('request');
 }).
 factory('vehicle', function(model) {
-	return model('vehicle');
+  return model('vehicle');
 });
 
